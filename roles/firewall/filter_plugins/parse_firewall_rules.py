3884106c370d35f738e22e3c6698f12efa2586d2
tekvalir <tekvalir@gmail.com>
2026-02-11 15:05:52 +0100

    refactor(roles/firewall): Split into 2 filters, one for validation and one for rule parsing
    Some pythons improvements also done:
        * Use 'isinstance' instead of 'type' for type validation
        * Use f-strings instead of %-strings
        * Use 'json.dumps' for adding comments contents to the rule builder
        * Type hints now use 'list' and 'dict' instead of importing from typing

--- roles/firewall/filter_plugins/parse_firewall_rules.py
+++ roles/firewall/filter_plugins/parse_firewall_rules.py
@@ -1,30 +1,31 @@
+import json
 import re
-from typing import Dict, List
+from typing import Any
 
 from ansible.errors import AnsibleFilterError
 from ansible.utils.display import Display
 
-regName = re.compile(r'\d{3}-\w+')
+rule_name = re.compile(r'\d{3}-\w+')
 
 # Build a single rule
-def parse_rule(rule: Dict[str, any]) -> str:
+def parse_firewall_rule(rule: dict[str, Any]) -> str:
     # Check that the rule is a dict
-    if type(rule) != dict:
-        raise AnsibleFilterError("Rules should be a dictionary!")
+    if not isinstance(rule, dict):
+        raise AnsibleFilterError(f"The rule \"{rule}\" should be a dictionary!")
     # If the argument raw_rule is present it defines completely the rule
     if "raw_rule" in rule:
         return rule["raw_rule"]
     builder = []
     if "chain" not in rule:
-        raise AnsibleFilterError("The rule does not have a 'chain' key!")
+        raise AnsibleFilterError(f"The rule \"{rule}\" must have a 'chain' key!")
     if "target" not in rule:
-        raise AnsibleFilterError("The rule does not have a 'target' key!")
+        raise AnsibleFilterError(f"The rule \"{rule}\" must have a 'target' key!")
     builder.extend(["-A", rule["chain"]])
     if "protocol" in rule:
         if rule["protocol"].lower() in ["tcp", "udp", "icmp", "all"]:
             builder.extend(["--protocol", rule["protocol"]])
         else:
-            raise AnsibleFilterError("The protocol \"%s\" is not supported!" % rule["protocol"])
+            raise AnsibleFilterError(f"The protocol \"{rule["protocol"]}\" in rule \"{rule}\" is not a valid protocol!")
     if "source" in rule:
         builder.extend(["--source", rule["source"]])
     if "destination" in rule:
@@ -37,51 +38,40 @@ def parse_rule(rule: Dict[str, any]) -> str:
         if "protocol" in rule and rule["protocol"].lower() in ["tcp", "udp"]:
             builder.extend(["--destination-port", str(rule["destination_port"])])
         else:
-            raise AnsibleFilterError("\"destination_port\" should be used with tcp or udp!")
+            raise AnsibleFilterError(f"The rule \"{rule}\" should not contain a 'destination_port' key. 'destination_port' should be used with tcp or udp!")
     builder.extend(["--jump", rule["target"]])
     if "comment" in rule:
-        builder.extend(["--match comment --comment", "\"" + rule["comment"] + "\""])
+        builder.extend(["--match comment --comment", json.dumps(rule["comment"])])
     else:
-        Display().warning("There are no comment present on the rule \"%s\"!" % rule)
+        Display().warning(f"There are no comment present on the rule \"{rule}\"! Commenting your rules is recommended.")
     if "raw_extras" in rule:
-        if type(rule["raw_extras"]) == list:
+        if isinstance(rule["raw_extras"], list):
             builder.extend(rule["raw_extras"])
-        elif type(rule["raw_extras"]) == str:
+        elif isinstance(rule["raw_extras"], str):
             builder.append(rule["raw_extras"])
         else:
-            raise AnsibleFilterError("The extras should be a list or a string!")
+            raise AnsibleFilterError(f"The extras in rule \"{rule}\" should be a list or a string!")
 
     return " ".join(builder)
 
-def parse_firewall_rules(rule_set: Dict[str, list]) -> Dict[str, list]:
-    # Check that the rule_set is a dict.
-    if type(rule_set) != dict:
-        raise AnsibleFilterError("The rule sets must be a dictionary!")
-
-    parsed_rule_set = {}
-
+def validate_firewall_rules(rule_set: dict[str, list]) -> dict[str, list]:
+    if not isinstance(rule_set, dict):
+        raise AnsibleFilterError("The firewall rule sets must be a dictionary!")
     for name, rules in rule_set.items():
         # Check that the rules are defined as a list.
-        if type(rules) != list:
-            raise AnsibleFilterError("%s should contain a list of rules!" % name)
+        if not isinstance(rules, list):
+            raise AnsibleFilterError(f"The rule set {name} should contain a list of rules!")
         # Check that the name of the rule set is formated correctly. Should be like 000-base.
-        if not regName.match(name):
-            raise AnsibleFilterError("%s is not a valid rule set name. Rule set name should match pattern '\\d{3}-\\w+'!" % name)
-        parsed_rules = []
-        # Loop on all rules to translate them into iptables rules.
+        if not rule_name.match(name):
+            raise AnsibleFilterError(f"{name} is not a valid rule set name! Rule set name should match pattern {rule_name.pattern}!")
         for rule in rules:
-            # We want to add some information if the underlying function except.
-            try:
-                parsed_rules.append(parse_rule(rule))
-            except AnsibleFilterError as err:
-                raise AnsibleFilterError("Rule \"%s\" in %s is invalid! %s" % (rule, name, err.message))
-        # Add the parsed rule in the dict to be returned.
-        parsed_rule_set[name] = parsed_rules
-
-    return parsed_rule_set
+            if not isinstance(rule, dict):
+                raise AnsibleFilterError(f"The rule \"{rule}\" in {name} rule set should be a dictionary!")
+    return rule_set
 
 class FilterModule(object):
     def filters(self):
         return {
-            'parse_firewall_rules':parse_firewall_rules
+            'validate_firewall_rules':validate_firewall_rules,
+            'parse_firewall_rule':parse_firewall_rule
         }
